e04efc470b9422d1cc851f550722d0e0dba59d1b
rebeca-august <rebeca.cazacu@gmail.com>
2021-01-27 20:03:41 +0000

    Finalise SingleColor component

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false)
+  const backgroundColor = rgb.join(',')
+  const hexValue = `#${hexColor}`
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setAlert(false)
+    }, 1000)
+    return () => clearTimeout(id)
+  }, [alert])
+
+  return (
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${backgroundColor})` }}
+      onClick={() => {
+        setAlert(true)
+        navigator.clipboard.writeText(hexValue)
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hexColor}</p>
+      {alert && <p className="alert">copied to the clipboard</p>}
+    </article>
+  )
 }
 
 export default SingleColor
